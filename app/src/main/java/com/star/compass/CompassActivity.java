e8f7cc8eb10d32e9954600f9ce7f2b9651adf1b8
crystalDf <chendong333@gmail.com>
2015-08-16 18:48:16 +0800

    Chapter 11 Compass finished fixed

--- app/src/main/java/com/star/compass/CompassActivity.java
+++ app/src/main/java/com/star/compass/CompassActivity.java
@@ -1,10 +1,10 @@
 package com.star.compass;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 
-public class CompassActivity extends ActionBarActivity {
+public class CompassActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
